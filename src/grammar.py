0491764c4844f3deb867061aa10596866a2f7acb
Ciprian Marin <ciprimarin2001@gmail.com>
2022-12-14 12:25:23 +0200

    separated functions

--- src/grammar.py
+++ src/grammar.py
@@ -65,90 +65,15 @@ class AugmentedGrammar(Grammar):
                 for prod in grammar.get_productions()[non_terminal]:
                     self._productions[non_terminal].append([Dot()] + prod)
 
-    def closure(self):
-        # List of states where each state is a dictionary that maps a starting nonterminal to a list
-        # where each element is a list of terminals and nonterminals
-        states = [self._productions]
+    @property
+    def productions(self):
+        return self._productions
 
-        productions = deepcopy(self._productions)
+    @property
+    def non_terminals(self):
+        return self._non_terminals
 
-        while True:
-            added_production = False
+    @property
+    def root_non_terminal(self):
+        return self._root_non_terminal
 
-            # Iterate all starting nonterminals and their lists of productions
-            for src, dst_list in productions.items():
-                # Iterate all productions in a nonterminal's production list
-                for dst in dst_list:
-                    # Find the index of the dot, if any
-                    dot_index = None
-
-                    for i, dst_item in enumerate(dst):
-                        if isinstance(dst_item, Dot):
-                            dot_index = i
-                            break
-
-                    # Skip the iteration if the production doesn't contain a dot
-                    if dot_index is None:
-                        continue
-
-                    # Skip the iteration if the dot is at the end of the production
-                    next_index = dot_index + 1
-                    if next_index >= len(dst):
-                        continue
-
-                    # Generate a production where the dot and the next item are swapped
-                    swapped_production = (
-                        dst[:dot_index]
-                        + [dst[next_index], Dot()]
-                        + dst[(dot_index + 2) :]
-                    )
-
-                    # Skip the iteration if the production already exists
-                    if swapped_production in productions[src]:
-                        continue
-
-                    # Add the production and set the flag to continue iterations
-                    productions[src].append(swapped_production)
-                    added_production = True
-
-                    # Create a new state to add to the result
-                    state = {}
-                    state[src] = [swapped_production]
-
-                    non_terminal_index = next_index + 1
-
-                    # If the item right after the dot is a nonterminal, add its productions to the
-                    # state
-                    if (
-                        non_terminal_index < len(swapped_production)
-                        and swapped_production[non_terminal_index]
-                        in self._non_terminals
-                    ):
-                        non_terminal = swapped_production[non_terminal_index]
-                        state[non_terminal] = []
-
-                        for dst in self._productions[non_terminal]:
-                            if dst != swapped_production:
-                                state[non_terminal].append(dst)
-
-                    states.append(state)
-
-            if not added_production:
-                break
-
-        return states
-
-
-g = Grammar()
-g.load_from_file("../docs/simple-syntax.json")
-ag = AugmentedGrammar(g, "S")
-result = ag.closure()
-
-for i, state in enumerate(result):
-    print(f"[STATE {i}]")
-
-    for src, dst_list in state.items():
-        for dst in dst_list:
-            print(f"{src} -> {dst}")
-
-    print()
